eef3093722fe3d8cb1ab381f88f966a7121ebde3
Thanh Dat <tranlethanhdat1995@gmail.com>
2019-05-29 23:20:01 +0700

    Show Posts & Categories & Topics

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,77 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#Create Categories
+Category.create!(name: "Chuyện Coding")
+Category.create!(name: "Hôm nay học gì?")
+Category.create!(name: "Vài Chuyện Tinh Linh")
+
+#Create Topics
+Topic.create!(category_id: "1",
+              name: "Series Chuyện Đi Học")
+Topic.create!(category_id: "1",
+              name: "Series Chuyện Thực Tập")
+Topic.create!(category_id: "1",
+              name: "Series Chuyện Đi Làm")
+Topic.create!(category_id: "2",
+              name: "Series Hướng Dẫn Chơi Guitar")
+Topic.create!(category_id: "2",
+              name: "Series Lập Trình Ruby")
+Topic.create!(category_id: "2",
+              name: "Series Lập Trình Android")
+Topic.create!(category_id: "3",
+              name: "Review sách")
+Topic.create!(category_id: "3",
+              name: "Review Vài Bộ Phim Boom Tấn")
+Topic.create!(category_id: "3",
+              name: "Tán Gái Truyền Kì")
+Topic.create!(category_id: "3",
+              name: "Điểm Hẹn Hò Ở Đà Nẵng")
+
+#Create Posts
+3.times do |n|
+  content = Faker::Lorem.sentence(20)
+  Post.create!(topic_id: "1",
+               title: "Chuyện Đi Học Cấp #{n+1}",
+               content: content,
+               image: "viet-nam.jpg")
+end
+3.times do |n|
+  content = Faker::Lorem.sentence(20)
+  Post.create!(topic_id: "2",
+               title: "Chuyện Thực Tập FSoft #{n+1}",
+               content: content,
+               image: "viet-nam.jpg")
+end
+3.times do |n|
+  content = Faker::Lorem.sentence(20)
+  Post.create!(topic_id: "3",
+               title: "Chuyện Đi Làm Framgia #{n+1}",
+               content: content,
+               image: "viet-nam.jpg",
+               views: "#{n+10}")
+end
+3.times do |n|
+  title = "Guitar Căn Bản #{n+1}"
+  content = Faker::Lorem.sentence(20)
+  Post.create!(topic_id: "4",
+               title: title,
+               content: content,
+               image: "viet-nam.jpg",
+               views: "#{n+15}")
+end
+3.times do |n|
+  title = "Ruby Căn Bản #{n+1}"
+  content = Faker::Lorem.sentence(20)
+  Post.create!(topic_id: "5",
+               title: title,
+               content: content,
+               image: "viet-nam.jpg",
+               views: "#{n+9}")
+end
+3.times do |n|
+  title = "Nguyễn Nhật Ánh #{n+1}"
+  content = Faker::Lorem.sentence(20)
+  Post.create!(topic_id: "7",
+               title: title,
+               content: content,
+               image: "viet-nam.jpg",
+               views: "#{n+10}")
+end
