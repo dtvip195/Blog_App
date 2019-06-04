d6b8bb45566c0b8d33a630baf990dcf9a049eb11
Thanh Dat <tranlethanhdat1995@gmail.com>
2019-06-04 13:56:09 +0700

    Post Details

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
-    @new_post = Post.order_new_posts.paginate(page: params[:page], per_page: 2)
+    @new_post = Post.order_new_posts
+                    .paginate(page: params[:page],
+                      per_page: Settings.post.per_page)
     respond_to do |format|
       format.html
       format.js
